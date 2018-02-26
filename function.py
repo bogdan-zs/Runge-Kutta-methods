a4d04425ac052028477f178e8205f5df578e4a06
Bogdan Zaseka <zaseka.bogdan@gmail.com>
2018-02-26 21:17:14 +0300

    refactoring code

--- function.py
+++ function.py
@@ -1,8 +1,5 @@
 import numpy as np
 
-n = 2  # systems
-
-
 def result_for_method(f, h, yk):
     y = yk + (h / 2.) * f
     return y + (h / 2.) * y
@@ -37,40 +34,44 @@ def result_derivative(x, y, f):
     return f(x, y)
 
 
-def result_rynge_kyta(a, b, h, f_arr):
+def result_rynge_kyta(a, b, h, arr_func, first_step):
     yp = []
-    yk = [0.0, 1.0]
+    yk = []
+    yk += first_step
     yp.append(yk)
+    count_func = len(arr_func)
+
     while a <= b:
         k1_arr = []
         k2_arr = []
         k3_arr = []
         k4_arr = []
-        for i in range(n):
-            res = result_derivative(a, yk[i], f_arr[i])
+        for i in range(count_func):
+            res = result_derivative(a, yk[i], arr_func[i])
             k1_arr.append(h * res)  # k1
 
-        for i in range(n):
-            res = yk[i] + k1_arr[i] / 2.
-            k2_arr.append(h * result_derivative(a + h / 2., res, f_arr[i]))  # k2
+        for i in range(count_func):
+            res = result_derivative(a + h / 2., yk[i] + k1_arr[i] / 2., arr_func[i])
+            k2_arr.append(h * res)  # k2
 
-        for i in range(n):
-            res = yk[i] + k2_arr[i] / 2.
-            k3_arr.append(h * result_derivative(a + h / 2., res, f_arr[i]))  # k3
+        for i in range(count_func):
+            res = result_derivative(a + h / 2., yk[i] + k2_arr[i] / 2., arr_func[i])
+            k3_arr.append(h * res)  # k3
 
-        for i in range(n):
-            res = yk[i] + k3_arr[i]
-            k4_arr.append(h * result_derivative(a + h, res, f_arr[i]))  # k4
+        for i in range(count_func):
+            res = result_derivative(a + h, yk[i] + k3_arr[i], arr_func[i])
+            k4_arr.append(h * res)  # k4
 
         yk = result_with_arr_k(k1_arr, k2_arr, k3_arr, k4_arr, yk)
 
         a += h
         yp.append(yk)
-    return yp              # return list[list[y11...y1n],list[y21....y2n]]
+    return yp  # return list[list[y11...y1n],list[y21....y2n]]
 
 
 def result_with_arr_k(k1_arr, k2_arr, k3_arr, k4_arr, yk):
     y_finish = []
-    for i in range(n):
+    count_func = len(yk)
+    for i in range(count_func):
         y_finish.append(yk[i] + k1_arr[i] / 6. + k2_arr[i] / 3. + k3_arr[i] / 3. + k4_arr[i] / 6.)
     return y_finish
